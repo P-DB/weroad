e5dba0632f5f1d74a07a128f84c845b269ade412
pdibartolomeo <pdibartolomeo@soldo.com>
2022-03-08 17:02:23 +0100

    footer desktop

--- src/components/footer/Footer.style.tsx
+++ src/components/footer/Footer.style.tsx
@@ -1,15 +1,20 @@
 const FooterStyle = {
-    footer:'flex flex-col text-white bg-footer-sm bg-cover',
+    footer:'flex flex-col text-white bg-footer-sm bg-cover lg:bg-footer-lg',
     gradient:'bg-gradient-to-t from-dark-gray to-dark-gray-80',
-    container:'flex flex-col w-full items-center max-w-6xl mx-auto px-3 py-4 lg:flex-row lg:justify-between',
+    container:'flex flex-col w-full items-center max-w-6xl mx-auto px-3 py-4 lg:flex-row lg:justify-between lg:items-start lg:mt-7',
     logo:'mt-4 lg:m-0',
-    inputContainer:'flex items-center justify-center w-full mt-7',
+    inputContainer:'flex items-center justify-center w-full min-w-0 mt-7 lg:min-w-[495px] lg:m-0',
     input:'flex-1 p-2 text-gray-3 rounded-l-md',
     button:'rounded-l-none',
     info:'text-center mt-4',
     infoSuggestion:'block text-sm text-gray-3',
-    social:'flex items-center my-9 lg:m-0',
+    social:'flex items-center my-9 lg:mb-0 mt-4',
     instagram:'mx-6',
+    links:'hidden max-w-6xl mx-auto my-12 px-3 lg:flex',
+    col:'flex-1',
+    linkSection:'mb-5',
+    linkTitle:'text-white text-semibold mb-2',
+    linkItem:'text-primary text-sm hover:cursor-pointer',
     disclaimer: 'bg-dark-gray py-5 px-4 text-xs text-center'
 }
 
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,6 +6,137 @@ import MyButton from "components/shared/myButton/MyButton";
 import style from "./Footer.style";
 
 function Footer() {
+  const linkFooter = [
+    [
+      {
+        title: "INVERNO SULLA NEVE",
+        links: [
+          "Austria",
+          "Bormio",
+          "Bardonecchia",
+          "Canazei",
+          "Cervinia",
+          "Dolomiti",
+          "Dolomiti Express",
+          "Monte Civetta",
+          "Plan de Corones",
+          "Prato Nevoso",
+          "Sestriere",
+          "Trentino",
+        ],
+      },
+      {
+        title: "PRIMAVERA IN EUROPA",
+        links: [
+          "Canarie",
+          "Guadeloupe",
+          "Portogallo",
+          "Scozia",
+          "Belgio",
+          "Polonia",
+          "Andalusia",
+          "Olanda",
+        ],
+      },
+    ],
+    [
+      {
+        title: "UTILIZZA VOUCHER COVID",
+        links: ["Prenota per il 2021"],
+      },
+      {
+        title: "2021 IN GIRO PER IL MONDO",
+        links: [
+          "Asia",
+          "Africa",
+          "Europa",
+          "Nord America",
+          "Centro America",
+          "Sud America",
+          "Middle East",
+          "Oceania",
+        ],
+      },
+      {
+        title: "2021 IN ITALIA",
+        links: [
+          "Barca a vela",
+          "Campania",
+          "Sicilia",
+          "Sardegna",
+          "Toscana",
+          "Puglia",
+          "Sicilia",
+          "Toscana",
+        ],
+      },
+    ],
+    [
+      {
+        title: "VIAGGI EXPRESS EUROPA",
+        links: [
+          "Belgio",
+          "Edimburgo",
+          "Portogallo",
+          "Vienna e Praga",
+          "Stoccolma",
+          "San Pietroburgo",
+          "Sofia",
+        ],
+      },
+      {
+        title: "VIAGGI EXPRESS ITALIA",
+        links: [
+          "Orvieto e Civita di Bagnoregio",
+          "Venezia",
+          "Toscana Express",
+          "Napoli e Pompei",
+          "Matera e Alberobello",
+          "Umbria Express",
+        ],
+      },
+    ],
+    [
+      {
+        title: "IL MONDO WEROAD",
+        links: [
+          "La formula WeRoad",
+          "Fasce d'età",
+          " Turni speciali",
+          "Mood",
+          "Il buon WeRoader",
+          "FAQ",
+          " Tutti i viaggi",
+          "Coordinatori",
+          "Contatti",
+          "Lavora con noi",
+          "Termini e condizioni",
+          "Privacy Policy",
+          "Cookie Policy",
+          "Company Profile",
+        ],
+      },
+    ],
+    [
+      {
+        title: "DIVENTA COORDINATORE",
+        links: ["Vita da Coordinatore", "Fai application", "Accedi"],
+      },
+      {
+        title: "CARTE REGALO WEROAD",
+        links: ["Scopri le gift card WeRoad"],
+      },
+      {
+        title: "T-SHIRT & GADGET",
+        links: ["Collection CASA 360°"],
+      },
+      {
+        title: "PARTNERS AND DMCS",
+        links: ["Become our partner"],
+      },
+    ],
+  ];
+
   return (
     <footer className={style.footer}>
       <div className={style.gradient}>
@@ -38,13 +169,32 @@ function Footer() {
             <IconFacebook />
           </div>
         </div>
+        <div className={style.links}>
+          {linkFooter.map((item, i) => (
+            <div key={i} className={style.col}>
+              {item.map((el, i) => (
+                <div className={style.linkSection} key={i}>
+                  <h6 className={style.linkTitle}>{el.title}</h6>
+                  <ul>
+                    {el.links.map((link, i) => (
+                      <li key={i} className={style.linkItem}>
+                        {link}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
         <div className={style.disclaimer}>
           <small>
             Questo sito è proprietà di WeRoad S.r.l. - Viale Cassala, 30 - 20143
             Milano (MI) | Cod. fiscale e P.IVA 10380820968 | Capitale sociale
             Euro 64.267,00 € i.v. | Registro Imprese Milano 08/06/2018
-            n°10380820968 | Num R.E.A.: MI-2526851 WeRoad S.r.l. (part of
-            OneDay) - 02.89950522 - Pagamenti sicuri via Stripe
+            n°10380820968 | Num R.E.A.: <br />
+            MI-2526851 WeRoad S.r.l. (part of OneDay) - 02.89950522 - <br />
+            Pagamenti sicuri via Stripe
           </small>
         </div>
       </div>
